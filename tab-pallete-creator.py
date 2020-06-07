90f8dc80f93280e037f8c383f511c97afb472888
Ben <66370595+bennnrenshaw@users.noreply.github.com>
2020-06-07 17:05:14 +0200

    Update tab-pallete-creator.py

--- tab-pallete-creator.py
+++ tab-pallete-creator.py
@@ -1,11 +1,18 @@
 """
-    tab-palette-creator
-    
-    Create a palette using colorthief 
-    and write directly to the preferences.tps file in
-    Tableau
 
-"""
+Take File Path or URL and create Tableau Colour Palette
+Choose between 1 and 10 colours for the Palette
+Colour Palette Quality = 1 - takes longer but high quality
+Colour Palette Quality = 10 - quicker but lower quality
+
+-- This version includes an IF statement to choose between a File Path or URL 
+-- File Path is translated into URL form
+
+Created using colorthief
+
+Ben Renshaw
+
+""" 
 
 
 def rgb_to_hex(rgb): 
@@ -31,63 +38,169 @@ def replace_line(file_name, line_num, text):
     out = open(file_name, 'w')
     out.writelines(lines)
     out.close()
-    #read file, write lines and close file 
+    #read file, write lines and close file
+
+def path2url(path):
+    return urljoin('file:', pathname2url(path))
 
 
-preferences = '#FILE PATH'
+preferences = ''
 #this should  be your file path to Preferences.tps file - don't forget \\ 
 subend = '</preferences>'
 #end of Preferences.tps file 
 substart = '</color-palette>'
 #end of last colour palette imported
 import sys
+import os
 from urllib.request import urlopen
 import io
 from colorthief import ColorThief
+from urllib.request import pathname2url
+from urllib.parse import urljoin
 
-b = input('Please Enter An URL:\n')
-#input URL of photo 
-d = input('Please Enter Palette Size between 1-10:\n')
+
+
+urlorfilepath = input('Choose URL or File Path:\n')
+#Enter URL or File Path to take you on that Path
+
+
+if urlorfilepath == 'File Path':
+        b = input('Please Enter An File Path:\n')
+#input filepath of photo 
+        d = input('Please Enter Palette Size between 1-10:\n')
 #colorthief allows for a palette size of 1-10, you can choose the size here 
-cc = int(d)
+        cc = int(d)
 #ensures that palette size input is an integer
-e = input('Please Enter a Palette Quality (Highest Quality = 1, Lowest Quality = 10):\n')
+        e = input('Please Enter a Palette Quality (Highest Quality = 1, Lowest Quality = 10):\n')
 #colortheif allows for a palette quality, 1 is highest, 10 is lowest - the lower the quality the faster it is returned
-ee = int(e)
+        ee = int(e)
 #ensures that palette quality is an integer
-url = urlopen(b)
-#opens given URL
-pic = io.BytesIO(url.read())
+        url = urlopen(path2url(b))
+#turns FilePath from FilePath into URL using path2url function
+        pic = io.BytesIO(url.read())
 #transforms URL into bytes - or actually UTF-8 
-colour_thief = ColorThief(pic)
+        colour_thief = ColorThief(pic)
 #import bytes into colour thief 
 #single = (colour_thief.get_color(quality=1)) can use this input to get single, dominant colour 
-palette = (colour_thief.get_palette(quality=ee, color_count = cc, ignore_white=True))
+        palette = (colour_thief.get_palette(quality=ee, color_count = cc, ignore_white=True))
 #this returns palette with quality & colour count from earlier inputs. ignore_white currently hardcoded 
-list=[]
+        list=[]
 #empty list
-for item in palette:
-        list.append('<color>'+'#'+rgb_to_hex(item)+'</color>')
+        for item in palette:
+            list.append('<color>'+'#'+rgb_to_hex(item)+'</color>')
 #For loop to transform RGB tuple to Hex code, and inserting necessary parts for preferences file 
-c = input('Name your Pallete:\n')
+        c = input('Name your Pallete:\n')
 #name inputted here will be name in Tableau
-cpname = '<color-palette name='+'"'+c+'"'+' type="regular">'
+        cpname = '<color-palette name='+'"'+c+'"'+' type="regular">'
 #top part of colour palette in preferences file 
-cpend = '</color-palette>'
+        cpend = '</color-palette>'
 #end of colour palette in preferences file 
-new_list = str(list).replace("['","").replace("']","").replace("', '","")
+        new_list = str(list).replace("['","").replace("']","").replace("', '","")
+#transforms list into one large string, removing punctuation, etc. 
+        colour_palette = cpname+new_list+cpend
+#create colour palette, ready to be written to preferences.tps 
+        start = (string_search(preferences, substart))
+#finds line number(s) of end of colour palettes 
+        end = (string_search(preferences, subend))
+#finds end of Preferences file 
+        new_line = ((start[-1])+1)
+#finds last colour palette using index -1, then adds one so it writes on line below 
+        replace_line(preferences, new_line, colour_palette)
+#replaces empty lines in preferences file with colour palette 
+        print('Open Tableau & Enjoy your new palette')
+elif urlorfilepath == 'URL':
+        b = input('Please Enter An URL:\n')
+#input URL of photo 
+        d = input('Please Enter Palette Size between 1-10:\n')
+#colorthief allows for a palette size of 1-10, you can choose the size here 
+        cc = int(d)
+#ensures that palette size input is an integer
+        e = input('Please Enter a Palette Quality (Highest Quality = 1, Lowest Quality = 10):\n')
+#colortheif allows for a palette quality, 1 is highest, 10 is lowest - the lower the quality the faster it is returned
+        ee = int(e)
+#ensures that palette quality is an integer
+        url = urlopen(b)
+#opens given URL
+        pic = io.BytesIO(url.read())
+#transforms URL into bytes - or actually UTF-8 
+        colour_thief = ColorThief(pic)
+#import bytes into colour thief 
+#single = (colour_thief.get_color(quality=1)) can use this input to get single, dominant colour 
+        palette = (colour_thief.get_palette(quality=ee, color_count = cc, ignore_white=True))
+#this returns palette with quality & colour count from earlier inputs. ignore_white currently hardcoded 
+        list=[]
+#empty list
+        for item in palette:
+            list.append('<color>'+'#'+rgb_to_hex(item)+'</color>')
+#For loop to transform RGB tuple to Hex code, and inserting necessary parts for preferences file 
+        c = input('Name your Pallete:\n')
+#name inputted here will be name in Tableau
+        cpname = '<color-palette name='+'"'+c+'"'+' type="regular">'
+#top part of colour palette in preferences file 
+        cpend = '</color-palette>'
+#end of colour palette in preferences file 
+        new_list = str(list).replace("['","").replace("']","").replace("', '","")
 #transforms list into one large string, removing punctuation, etc. 
    
-colour_palette = cpname+new_list+cpend
+        colour_palette = cpname+new_list+cpend
 #create colour palette, ready to be written to preferences.tps 
 
-start = (string_search(preferences, substart))
+        start = (string_search(preferences, substart))
 #finds line number(s) of end of colour palettes 
-end = (string_search(preferences, subend))
+        end = (string_search(preferences, subend))
 #finds end of Preferences file 
-new_line = ((start[-1])+1)
+        new_line = ((start[-1])+1)
 #finds last colour palette using index -1, then adds one so it writes on line below 
 
-replace_line(preferences, new_line, colour_palette)
+        replace_line(preferences, new_line, colour_palette)
 #replaces empty lines in preferences file with colour palette 
-print('Open Tableau & Enjoy your new palette')
+        print('Open Tableau & Enjoy your new palette')
+
+elif urlorfilepath == 'File Path':
+        b = input('Please Enter An File Path:\n')
+#input URL of photo 
+        d = input('Please Enter Palette Size between 1-10:\n')
+#colorthief allows for a palette size of 1-10, you can choose the size here 
+        cc = int(d)
+#ensures that palette size input is an integer
+        e = input('Please Enter a Palette Quality (Highest Quality = 1, Lowest Quality = 10):\n')
+#colortheif allows for a palette quality, 1 is highest, 10 is lowest - the lower the quality the faster it is returned
+        ee = int(e)
+#ensures that palette quality is an integer
+        url = urlopen(path2url(b))
+#opens given URL
+        pic = io.BytesIO(url.read())
+#transforms URL into bytes - or actually UTF-8 
+        colour_thief = ColorThief(pic)
+#import bytes into colour thief 
+#single = (colour_thief.get_color(quality=1)) can use this input to get single, dominant colour 
+        palette = (colour_thief.get_palette(quality=ee, color_count = cc, ignore_white=True))
+#this returns palette with quality & colour count from earlier inputs. ignore_white currently hardcoded 
+        list=[]
+#empty list
+        for item in palette:
+            list.append('<color>'+'#'+rgb_to_hex(item)+'</color>')
+#For loop to transform RGB tuple to Hex code, and inserting necessary parts for preferences file 
+        c = input('Name your Pallete:\n')
+#name inputted here will be name in Tableau
+        cpname = '<color-palette name='+'"'+c+'"'+' type="regular">'
+#top part of colour palette in preferences file 
+        cpend = '</color-palette>'
+#end of colour palette in preferences file 
+        new_list = str(list).replace("['","").replace("']","").replace("', '","")
+#transforms list into one large string, removing punctuation, etc. 
+   
+        colour_palette = cpname+new_list+cpend
+#create colour palette, ready to be written to preferences.tps 
+
+        start = (string_search(preferences, substart))
+#finds line number(s) of end of colour palettes 
+        end = (string_search(preferences, subend))
+#finds end of Preferences file 
+        new_line = ((start[-1])+1)
+#finds last colour palette using index -1, then adds one so it writes on line below 
+
+        replace_line(preferences, new_line, colour_palette)
+#replaces empty lines in preferences file with colour palette 
+        print('Open Tableau & Enjoy your new palette')
+os.system('pause') 
